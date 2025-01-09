52f2f481acf49e50e498137b6005ee08cb351f4e
Hubli-Sushmita <125744066+Sushmita-Hubli@users.noreply.github.com>
2025-01-09 15:48:44 -0500

    Basics of Array initialization and declaration

---
+++ DSA IN Java/src/Arrays/Array1.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class Array1
+{
+    public static void main(String[] args) {
+        System.out.println("An array is a data structure that stores a fixed-size collection of elements, all of the same type, in a contiguous block of memory. Each element can be accessed using an index, with the first element at index 0. Arrays allow for efficient access to elements but have a fixed size once created.");
+        System.out.println("There are two ways for Declaration of array: \n1.int myArray[]\n2.int[] myArray");
+        System.out.println("Initialization of an Array: It gives memory to array elements. \narrayName= new dataType[size]   \neg: myArray= new int[5]");
+        System.out.println();
+        System.out.println("Declaration and Initialization of Array can be done both together in three ways as follows:\n1. int arr[]=new int[5];\n2. int[] arr=new int[5];\n3. int arr={1,2,3,4,5};");
+
+
+    }
+}
