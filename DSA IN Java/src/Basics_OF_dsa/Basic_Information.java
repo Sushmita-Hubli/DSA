5c895622dd8b548d22276accb0f30dd48fe4111c
Hubli-Sushmita <125744066+Sushmita-Hubli@users.noreply.github.com>
2025-01-09 13:57:04 -0500

    basic information about data structures and algorithms

---
+++ DSA IN Java/src/Basics_OF_dsa/Basic_Information.java
@@ -0,0 +1,28 @@
+package Basics_OF_dsa;
+
+public class Basic_Information
+{
+    public static void main(String[] args) {
+        System.out.println("Data Structures are used to organize data in a specific manner in a sorted way so as to perform certain operations on the data");
+        System.out.println("Data Structures are of two types: 1. Linear(Arrays, LinkedList,Stacks and Queues) and 2. Non Linear(Trees and Graphs)");
+        System.out.println("A linear data structure organizes elements in a sequential order, where each element is connected to its previous and next elements (e.g., arrays, linked lists), whereas a non-linear data structure organizes elements hierarchically, allowing multiple connections, creating a complex relationship among elements (e.g., trees, graphs).");
+        System.out.println("An algorithm is a step-by-step procedure or set of rules designed to perform a specific task or solve a problem efficiently.");
+        System.out.println();
+        System.out.println("Analysis of Algorithm deals in finding best algorithm which runs fast and takes in less memory.");
+        System.out.println();
+        System.out.println("Time Complexity: Time complexity is how long an algorithm takes to run as the size of the input increases.\n" +
+                "\n" +
+                "Space Complexity: Space complexity is how much memory an algorithm needs to run as the size of the input increases.");
+
+        System.out.println();
+        System.out.println("Asymptotic Ananlysis of an Algorithm is the process of evaluating how the performance of an algorithm (in terms of time and space) changes as the size of the input grows, especially for very large inputs. It helps us understand the efficiency of an algorithm in the long run and compare different algorithms.\n" +
+                "\n" +
+                "There are three main types of asymptotic analysis:\n" +
+                "\n" +
+                "Big O (O): Represents the upper bound of an algorithm's growth rate, showing the worst-case scenario (how the algorithm performs in the worst possible input).\n" +
+                "Omega (Ω): Represents the lower bound, showing the best-case scenario (how the algorithm performs in the best possible input).\n" +
+                "Theta (Θ): Represents both the upper and lower bounds, giving the exact asymptotic behavior of an algorithm. ");
+        System.out.println();
+        System.out.println("Asymptotic Notations are mathemtical tools used to describe the running time of an algorithm in terms of input size.\n Asymptotic Notations help us in determining:\n1.Best Case\n2.Average Case\n3.Worst Case");
+    }
+}
