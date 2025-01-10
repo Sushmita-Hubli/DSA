abae6a00182d7ddb238c4d98926886b2039681f0
Hubli-Sushmita <125744066+Sushmita-Hubli@users.noreply.github.com>
2025-01-10 11:51:57 -0500

    creating a Singly LinkedList

---
+++ DSA IN Java/src/LinkedList/LL1.java
@@ -0,0 +1,10 @@
+package LinkedList;
+
+public class LL1
+{
+    public static void main(String[] args) {
+        System.out.println("Basic info about Singly Linked List\n" +
+                "A singly linked list is a linear data structure where each element (node) contains data and a pointer to the next node, with the last node pointing to null.");
+
+    }
+}
---
+++ DSA IN Java/src/LinkedList/LL2.java
@@ -0,0 +1,29 @@
+package LinkedList;
+
+
+public class LL2
+{
+    
+    private static class ListNode{
+
+        private int data;
+        private ListNode next; //pointer to the next node
+
+        public ListNode(int data)
+        {
+            this.data=data;
+            next=null;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Creation of singly Linked List");
+        ListNode head=new ListNode(10);
+        ListNode second=new ListNode(9);
+        head.next=second;
+        ListNode third=new ListNode(8);
+        second.next=third;
+        ListNode fourth=new ListNode(7);
+        third.next=fourth;
+
+    }
+}
